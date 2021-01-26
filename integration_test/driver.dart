c5a4658dc760f237e92bd13fefd196098a3701cc
done <23427957+sensuikan1973@users.noreply.github.com>
2021-01-27 01:40:03 +0900

    setup as flutter project. (#1)
    - CI
      -flutter format/analyze/test
      - test coverage report
    - README
    - required package "libedax4dart"
    
    references
    - https://github.com/sensuikan1973/dart-boilerplate/blob/main/.github/workflows/dart_ci.yaml
    - https://github.com/sensuikan1973/HumanLifeGame/tree/master/.github/workflows
    - https://github.com/sensuikan1973/libedax4dart

--- integration_test/driver.dart
+++ integration_test/driver.dart
@@ -3,6 +3,8 @@
 //
 // flutter drive --driver integration_test/driver.dart --target integration_test/app_test.dart
 
+// See: The library 'package:integration_test/integration_test.dart' is legacy,
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:integration_test/integration_test_driver.dart';
 
-Future<void> main() => integrationDriver();
+Future<void> main() async => integrationDriver();
