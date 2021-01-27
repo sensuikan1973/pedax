08ca099aaa842465547dabc97dcaf069e8b12258
done <23427957+sensuikan1973@users.noreply.github.com>
2021-01-27 21:15:31 +0900

    add integration test job (#6)

--- integration_test/driver.dart
+++ integration_test/driver.dart
@@ -3,8 +3,9 @@
 //
 // flutter drive --driver integration_test/driver.dart --target integration_test/app_test.dart
 
+// @dart = 2.11
 // See: https://github.com/flutter/flutter/issues/71379
-// ignore: import_of_legacy_library_into_null_safe
+// See: https://dart.dev/null-safety/unsound-null-safety
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() async => integrationDriver();
