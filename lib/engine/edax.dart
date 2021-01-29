316c828fc878ed4f670c462c5e83746e5d83025f
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-29 09:22:20 +0900

    engine の I/F 初期of初期実装

---
+++ lib/engine/edax.dart
@@ -0,0 +1,45 @@
+// @dart = 2.11
+// path_provider が null safety でないため
+// See: https://dart.dev/null-safety/unsound-null-safety
+
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart'; // 使う
+
+@immutable
+class edax {
+  edax() {
+    // TODO: 実装
+    // loadPreferences で book, eval, dll の path を入手し、edax を起動する
+    // preferences が無い/定義が無い場合は、Asset Bundle から「book, eval, dll, 設定ファイル(yaml かな? 扱いやすければ何でもok)」をコピってきて、そこを参照するようにする
+  }
+
+  setBookPath() {
+    // TODO: 実装
+    // updatePreferences()
+    // 後続の処理で book load が必要
+    // libedax4dart にそもそも book_load の binding をまだ実装してないので、そっちの実装追加も必要
+    // まあ優先度低いので、アプリ自体の再起動が必要、というメッセージを明示しておく でもok。
+  }
+
+  setEvalPath() {
+    // TODO: 実装
+    // updatePreferences()
+    // アプリ自体の再起動が必要、というメッセージを明示しておく
+  }
+
+  setDllPath() {
+    // TODO: 実装
+    // updatePreferences()
+    // アプリ自体の再起動が必要、というメッセージを明示しておく
+  }
+
+  String loadPreferences() {
+    // TODO: 実装
+    // path_provider で platform に応じた領域を参照する
+  }
+
+  void updatePreferences() {
+    // TODO: 実装
+    // path_provider で platform に応じた領域へ書き込む
+  }
+}
