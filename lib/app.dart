2eb41c3c76c32ae6267f5780c4ed15b0ef2bbb1f
ShimizuNaoki <sensuikan1973@gmail.com>
2021-02-02 21:08:32 +0900

    put libedax.dylib in xcodebuild. See: https://flutter.dev/docs/development/platform-integration/c-interop#compiled-dynamic-library-macos

--- lib/app.dart
+++ lib/app.dart
@@ -5,86 +5,21 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'engine/edax.dart' show tryToCallEdax;
+
+import 'home/home.dart';
 
 class PedaxApp extends StatelessWidget {
   const PedaxApp({Key key}) : super(key: key);
+
+  // static Widget inProviders({Key key}) => MultiProvider(
+  //       providers: [],
+  //       child: PedaxApp._(key: key),
+  //     );
 
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: 'pedax',
         theme: ThemeData(primarySwatch: Colors.green),
         home: const HomePage(title: 'pedax title'),
-      );
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(StringProperty('title', title));
-  }
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: FutureBuilder<String>(
-          future: tryToCallEdax(),
-          builder: (context, snapshot) {
-            final text = snapshot.connectionState != ConnectionState.done
-                ? 'You have pushed the button this many times:'
-                : snapshot.data;
-            return Center(
-              child: Column(
-                // Column is also a layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit its
-                // children horizontally, and tries to be as tall as its parent.
-                //
-                // Invoke "debug painting" (press "p" in the console, choose the
-                // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                // to see the wireframe for each widget.
-                //
-                // Column has various properties to control how it sizes itself and
-                // how it positions its children. Here we use mainAxisAlignment to
-                // center the children vertically; the main axis here is the vertical
-                // axis because Columns are vertical (the cross axis would be
-                // horizontal).
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(text),
-                  Text('$_counter', style: Theme.of(context).textTheme.headline4),
-                ],
-              ),
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
       );
 }
