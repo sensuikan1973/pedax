a4e070de8f6668cf175b9116b13981889f079284
done <23427957+sensuikan1973@users.noreply.github.com>
2021-02-03 12:55:11 +0900

    for now, not support to user-customized dll (#14)
    * not support to customize dll for now.
    * mild fix comment
    * add comment

--- lib/engine/edax.dart
+++ lib/engine/edax.dart
@@ -26,13 +26,6 @@ class Edax {
     // NOTE: require restart
   }
 
-  // NOTE: If you need, implement.
-  // Future<void> setDllPath(String path) async {
-  //   final pref = await _pref;
-  //   await pref.setString(libedaxPathPrefKey, path);
-  //   // require restart
-  // }
-
   Future<String> get bookPath async {
     final pref = await _pref;
     return pref.getString(bookFilePathPrefKey);
@@ -43,14 +36,9 @@ class Edax {
     return pref.getString(evalFilePathPrefKey);
   }
 
-  // NOTE: If you need, implement.
-  // Future<String> get dllPath async {
-  //   final pref = await _pref;
-  //   return pref.getString(libedaxPathPrefKey);
-  // }
-
   /// TODO: remove this Experiment function
-  /// ref: https://github.com/flutter/flutter/issues/17160
+  /// See: https://github.com/flutter/flutter/issues/17160
+  /// See: https://github.com/flutter/flutter/issues/28162
   Future<String> tryToCallEdax() async {
     final docDir = await _docDir;
     final pref = await _pref;
@@ -115,10 +103,6 @@ class Edax {
 
   @visibleForTesting
   static const evalFilePathPrefKey = 'evalFilePath';
-
-  // NOTE: If you need, implement.
-  // @visibleForTesting
-  // static const libedaxPathPrefKey = 'libedaxPath';
 
   @visibleForTesting
   static const defaultEvalFileName = 'eval.dat';
