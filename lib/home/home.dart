f8afa15294be942ddd747aeb69a9ad2ad0f7f9f2
ShimizuNaoki <sensuikan1973@gmail.com>
2021-02-06 19:44:17 +0900

    remove official example comment

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -35,36 +35,21 @@ class _HomePageState extends State<HomePage> {
           title: Text(AppLocalizations.of(context).homeTitle),
           actions: [
             IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: () async {
-                  final packageInfo = await PackageInfo.fromPlatform();
-                  // ignore: avoid_print
-                  print(packageInfo.version);
-                })
+              icon: const Icon(Icons.menu),
+              onPressed: () async {
+                final packageInfo = await PackageInfo.fromPlatform();
+                // ignore: avoid_print
+                print(packageInfo.version);
+              },
+            ),
           ],
         ),
         body: FutureBuilder<String>(
           future: _boardPrettyString,
-          builder: (context, snapshot) {
-            final text = snapshot.connectionState != ConnectionState.done
-                ? 'You have pushed the button this many times:'
-                : snapshot.data;
+          builder: (_, snapshot) {
+            final text = snapshot.connectionState != ConnectionState.done ? 'initializing libedax...' : snapshot.data;
             return Center(
               child: Column(
-                // Column is also a layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit its
-                // children horizontally, and tries to be as tall as its parent.
-                //
-                // Invoke "debug painting" (press "p" in the console, choose the
-                // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                // to see the wireframe for each widget.
-                //
-                // Column has various properties to control how it sizes itself and
-                // how it positions its children. Here we use mainAxisAlignment to
-                // center the children vertically; the main axis here is the vertical
-                // axis because Columns are vertical (the cross axis would be
-                // horizontal).
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[Text(text)],
               ),
