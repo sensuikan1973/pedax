03ae783b49697ea0e336be918b35ab8c0e0a1953
ShimizuNaoki <sensuikan1973@gmail.com>
2021-02-07 23:13:58 +0900

    test handle ResizeImage

--- test_helper/asset_image_finder.dart
+++ test_helper/asset_image_finder.dart
@@ -2,6 +2,11 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 Finder findByAssetKey(String key) => find.byWidgetPredicate((widget) {
-      if (widget is Image && widget.image is AssetImage) return (widget.image as AssetImage).keyName == key;
+      if (widget is! Image) return false;
+      if (widget.image is AssetImage) return (widget.image as AssetImage).keyName == key;
+      if (widget.image is ResizeImage) {
+        final resizeImage = widget.image as ResizeImage;
+        return (resizeImage.imageProvider as AssetImage).keyName == key;
+      }
       return false;
     });
