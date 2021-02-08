54e3be78aaecca15165928dadfc3bfe464ba1ac9
ShimizuNaoki <sensuikan1973@gmail.com>
2021-02-09 02:18:02 +0900

    update state

--- lib/board/pedax_board.dart
+++ lib/board/pedax_board.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:libedax4dart/libedax4dart.dart';
@@ -21,11 +23,29 @@ class PedaxBoard extends StatefulWidget {
 
 class _PedaxBoardState extends State<PedaxBoard> {
   late Board _board;
+  late List<int> _squaresOfPlayer;
+  late List<int> _squaresOfOpponent;
+  late int _currentColor;
+  late String _moves;
+  late List<Hint> _hints;
+  late Move? _lastMove;
+  late int? _bestScore;
 
   @override
   void initState() {
     super.initState();
+    updateState();
+  }
+
+  void updateState() {
     _board = widget.engine.edaxGetBoard();
+    _squaresOfPlayer = _board.squaresOfPlayer;
+    _squaresOfOpponent = _board.squaresOfOpponent;
+    _currentColor = widget.engine.edaxGetCurrentPlayer();
+    _moves = widget.engine.edaxGetMoves();
+    _lastMove = _moves.isEmpty ? null : widget.engine.edaxGetLastMove();
+    _hints = widget.engine.edaxHint(2); // for now, n is 2 and synchronous.
+    _bestScore = _hints.isEmpty ? null : _hints.map<int>((h) => h.score).reduce(max);
   }
 
   @override
@@ -50,49 +70,50 @@ class _PedaxBoardState extends State<PedaxBoard> {
   double get _stoneSize => (widget.length / _boardSize) - (_stoneMargin * 2);
 
   Square _square(int y, int x) {
-    final turn = widget.engine.edaxGetCurrentPlayer();
     final move = y * 8 + x;
-    final isBlackTurn = turn == TurnColor.black;
-    var type = SquareType.empty;
-    if (_board.squaresOfPlayer.contains(move)) {
-      type = isBlackTurn ? SquareType.black : SquareType.white;
-    } else if (_board.squaresOfOpponent.contains(move)) {
-      type = isBlackTurn ? SquareType.white : SquareType.black;
-    }
+    final type = _squareType(move);
     final moveString = move2String(move);
-    final moves = widget.engine.edaxGetMoves();
-    final hints = widget.engine.edaxHint(1).where((h) => h.move == move).toList();
-    final hint = hints.isEmpty ? null : hints.first;
-    final score = hint == null ? null : hints.first.score;
-
+    final targetHints = _hints.where((h) => h.move == move).toList();
+    final hint = targetHints.isEmpty ? null : targetHints.first;
     return Square(
       type: type,
       length: _stoneSize,
       margin: _stoneMargin,
       coordinate: moveString,
-      isLastMove: moves.isNotEmpty && widget.engine.edaxGetLastMove().x == move,
-      score: score,
-      scoreColor: _scoreColor(score, hint != null && hint.move == move),
-      onTap: () {
-        setState(() {
-          widget.engine.edaxMove(moveString);
-          _board = widget.engine.edaxGetBoard();
-        });
-      },
+      isLastMove: _lastMove?.x == move,
+      score: hint?.score,
+      scoreColor: _scoreColor(hint?.score, hint?.score == _bestScore),
+      onTap: type != SquareType.empty
+          ? null
+          : () => setState(() {
+                widget.engine.edaxMove(moveString);
+                updateState();
+              }),
     );
+  }
+
+  SquareType _squareType(int move) {
+    final isBlackTurn = _currentColor == TurnColor.black;
+    if (_squaresOfPlayer.contains(move)) {
+      return isBlackTurn ? SquareType.black : SquareType.white;
+    }
+    if (_squaresOfOpponent.contains(move)) {
+      return isBlackTurn ? SquareType.white : SquareType.black;
+    }
+    return SquareType.empty;
   }
 
   Color? _scoreColor(int? score, bool bestMove) {
     if (score == null) return null;
     if (score == 0) {
-      if (bestMove) return Colors.cyan[900]!;
-      return Colors.cyan;
+      if (bestMove) return Colors.cyan;
+      return Colors.cyan[900];
     } else if (score > 0) {
-      if (bestMove) return Colors.blue[900]!;
-      return Colors.blue;
+      if (bestMove) return Colors.blue;
+      return Colors.blue[900];
     } else /* score < 0*/ {
-      if (bestMove) return Colors.lime[900]!;
-      return Colors.lime;
+      if (bestMove) return Colors.lime;
+      return Colors.lime[900];
     }
   }
 }
