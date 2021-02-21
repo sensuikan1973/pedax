4c75073371f0a9a8c5f6d803b4a1566764d67fb3
ShimizuNaoki <sensuikan1973@gmail.com>
2021-02-22 00:37:30 +0900

    disable debugPrint on release mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
+  if (kReleaseMode) {
+    debugPrint = (message, {wrapWidth}) {};
+  }
   runApp(const PedaxApp());
 }
