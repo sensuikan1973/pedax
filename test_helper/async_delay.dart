dae4b31342a9679c18b12be202cc63db72fde6a8
ShimizuNaoki <sensuikan1973@gmail.com>
2021-02-22 22:14:38 +0900

    fix comment

--- test_helper/async_delay.dart
+++ test_helper/async_delay.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
 
-// EdaxServer process: less than 150 millisec with default book
+// EdaxServer process: less than 200 millisec with default book
 @isTest
 Future<void> delay200millisec(WidgetTester tester) async => Future<void>.delayed(const Duration(milliseconds: 200));
