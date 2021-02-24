3d99eb7525f60211f6751fa408d6eae3dee27083
done <23427957+sensuikan1973@users.noreply.github.com>
2021-02-24 20:05:21 +0900

    Update request_schema.dart

--- lib/engine/api/request_schema.dart
+++ lib/engine/api/request_schema.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 // We can pass `Object` instance to `EdaxServer port`.
 // It's because `EdaxServer` isolate and Flutter root isolate share the same code.
-// See: https://api.dart.dev/dev/2.13.0-38.0.dev/dart-isolate/Isolate/spawn.html
+// See: https://api.dart.dev/dev/dart-isolate/Isolate/spawn.html
 // See: https://api.dart.dev/dev/dart-isolate/SendPort/send.html
 
 @immutable
