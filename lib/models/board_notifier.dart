2b1e40ff45ea065ad3211a9a4989de716b601d97
ShimizuNaoki <sensuikan1973@gmail.com>
2021-02-27 21:02:59 +0900

    define BoardNotifier, BoardState

---
+++ lib/models/board_notifier.dart
@@ -0,0 +1,148 @@
+import 'dart:async';
+import 'dart:isolate';
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:logger/logger.dart';
+
+import '../engine/api/book_get_move_with_position.dart';
+import '../engine/api/book_load.dart';
+import '../engine/api/hint_one_by_one.dart';
+import '../engine/api/init.dart';
+import '../engine/api/move.dart';
+import '../engine/api/play.dart';
+import '../engine/api/redo.dart';
+import '../engine/api/undo.dart';
+import '../engine/edax_server.dart';
+import '../engine/options/hint_step_by_step_option.dart';
+import '../engine/options/level_option.dart';
+import 'board_state.dart';
+
+class BoardNotifier extends ValueNotifier<BoardState> {
+  BoardNotifier(BoardState value) : super(value);
+
+  final _logger = Logger();
+  final Completer<bool> _edaxServerSpawned = Completer<bool>();
+  late final SendPort _edaxServerPort;
+  final _receivePort = ReceivePort();
+  late final Stream<dynamic> _receiveStream;
+  final _hintstepByStepOption = const HintStepByStepOption();
+  final _levelOption = const LevelOption();
+
+  Future<void> spawnEdaxServer(String libedaxPath, List<String> initLibedaxParams) async {
+    await Isolate.spawn(
+      startEdaxServer,
+      StartEdaxServerParams(_receivePort.sendPort, libedaxPath, initLibedaxParams),
+    );
+    _receiveStream = _receivePort.asBroadcastStream();
+    _edaxServerPort = await _receiveStream.first as SendPort;
+    _edaxServerSpawned.complete(true);
+    _logger.d('spawned edax server');
+
+    _receiveStream.listen(_updateStateByEdaxServerResponse);
+    _edaxServerPort.send(const InitRequest());
+  }
+
+  void requestInit() => _edaxServerPort.send(const InitRequest());
+  void requestMove(String move) => _edaxServerPort.send(MoveRequest(move));
+  void requestPlay(String moves) => _edaxServerPort.send(PlayRequest(moves));
+  void requestUndo() => _edaxServerPort.send(const UndoRequest(times: 1));
+  void requestUndoAll() => _edaxServerPort.send(const UndoRequest(times: 60));
+  void requestRedo() => _edaxServerPort.send(const RedoRequest(times: 1));
+  void requestRedoAll() => _edaxServerPort.send(const RedoRequest(times: 60));
+
+  Future<void> switchHintVisibility() async {
+    value.hints.clear();
+    value.hintIsVisible = !value.hintIsVisible;
+    if (value.hintIsVisible) _edaxServerPort.send(await _buildHintRequest(value.currentMoves));
+  }
+
+  void requestBookLoad(String path) {
+    value.bookLoading = true;
+    _edaxServerPort.send(BookLoadRequest(path));
+  }
+
+  Future<HintOneByOneRequest> _buildHintRequest(String movesAtRequest) async => HintOneByOneRequest(
+        level: await _levelOption.val,
+        stepByStep: await _hintstepByStepOption.val,
+        movesAtRequest: movesAtRequest,
+      );
+
+  Future<void> _onMovesUpdated(String moves) async {
+    value.hints.clear();
+    if (value.hintIsVisible) _edaxServerPort.send(await _buildHintRequest(moves));
+    if (!value.bookLoading) _edaxServerPort.send(const GetBookMoveWithPositionRequest());
+  }
+
+  // ignore: avoid_annotating_with_dynamic
+  Future<void> _updateStateByEdaxServerResponse(dynamic message) async {
+    _logger.i('received response "${message.runtimeType}"');
+    if (message is MoveResponse) {
+      if (value.currentMoves != message.moves) await _onMovesUpdated(message.moves);
+      value
+        ..board = message.board
+        ..squaresOfPlayer = value.board.squaresOfPlayer
+        ..squaresOfOpponent = value.board.squaresOfOpponent
+        ..currentColor = message.currentColor
+        ..lastMove = message.lastMove
+        ..currentMoves = message.moves;
+    } else if (message is PlayResponse) {
+      if (value.currentMoves != message.moves) await _onMovesUpdated(message.moves);
+      value
+        ..board = message.board
+        ..squaresOfPlayer = value.board.squaresOfPlayer
+        ..squaresOfOpponent = value.board.squaresOfOpponent
+        ..currentColor = message.currentColor
+        ..lastMove = message.lastMove
+        ..currentMoves = message.moves;
+    } else if (message is InitResponse) {
+      if (!value.edaxInit.isCompleted) value.edaxInit.complete(true);
+      await _onMovesUpdated(message.moves);
+      value
+        ..board = message.board
+        ..squaresOfPlayer = value.board.squaresOfPlayer
+        ..squaresOfOpponent = value.board.squaresOfOpponent
+        ..currentColor = message.currentColor
+        ..lastMove = message.lastMove
+        ..currentMoves = message.moves;
+    } else if (message is UndoResponse) {
+      if (value.currentMoves != message.moves) await _onMovesUpdated(message.moves);
+      value
+        ..board = message.board
+        ..squaresOfPlayer = value.board.squaresOfPlayer
+        ..squaresOfOpponent = value.board.squaresOfOpponent
+        ..currentColor = message.currentColor
+        ..lastMove = message.lastMove
+        ..currentMoves = message.moves;
+    } else if (message is RedoResponse) {
+      if (value.currentMoves != message.moves) await _onMovesUpdated(message.moves);
+      value
+        ..board = message.board
+        ..squaresOfPlayer = value.board.squaresOfPlayer
+        ..squaresOfOpponent = value.board.squaresOfOpponent
+        ..currentColor = message.currentColor
+        ..lastMove = message.lastMove
+        ..currentMoves = message.moves;
+    } else if (message is HintOneByOneResponse) {
+      _logger.d('${message.hint.moveString}: ${message.hint.scoreString}');
+      if (message.request.movesAtRequest != value.currentMoves) return value.hints.clear();
+      value.hints
+        ..removeWhere((hint) => hint.move == message.hint.move)
+        ..add(message.hint);
+      value.bestScore = value.hints.map<int>((h) => h.score).reduce(max);
+    } else if (message is BookLoadResponse) {
+      // ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   SnackBar(
+      //     content: Text(AppLocalizations.of(context)!.finishedLoadingBookFile, textAlign: TextAlign.center),
+      //   ),
+      // );
+      value.bookLoading = false;
+    } else if (message is GetBookMoveWithPositionResponse) {
+      value
+        ..positionWinsNum = message.position.nWins
+        ..positionDrawsNum = message.position.nDraws
+        ..positionLossesNum = message.position.nLosses;
+    }
+  }
+}
---
+++ lib/models/board_state.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:libedax4dart/libedax4dart.dart';
+
+class BoardState {
+  BoardState();
+
+  late Board board;
+  late List<int> squaresOfPlayer;
+  late List<int> squaresOfOpponent;
+  late int currentColor;
+  late Move? lastMove;
+  late String currentMoves;
+  final List<Hint> hints = [];
+  final Completer<bool> edaxInit = Completer<bool>();
+
+  bool bookLoading = false;
+  bool hintIsVisible = true;
+  int bestScore = 0;
+  int positionWinsNum = 0;
+  int positionLossesNum = 0;
+  int positionDrawsNum = 0;
+
+  int get positionFullNum => positionWinsNum + positionLossesNum + positionDrawsNum;
+  int get positionWinsRate => (positionWinsNum / positionFullNum * 100).floor();
+  int get positionDrawsRate => (positionDrawsNum / positionFullNum * 100).floor();
+}
