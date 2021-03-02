3d32b3f482e80b3107017f378ddf446507ed663c
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-03 00:10:49 +0900

    setWindowMinSize

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 import 'app.dart';
+import 'window.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setWindowMinSize(pedaxWindowMinSize);
+
   runApp(const PedaxApp());
 }
