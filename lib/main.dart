b3010c559701b4516bcf309f8315b1532287a707
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-02 23:57:28 +0900

    rm unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
-  if (kReleaseMode) {
-    debugPrint = (message, {wrapWidth}) {};
-  }
   runApp(const PedaxApp());
 }
