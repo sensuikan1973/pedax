ece46433d64834bcfec0f960acd4bd0905b51905
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-08 07:50:44 +0900

    add macos_secure_bookmarks on dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,12 +8,14 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import macos_secure_bookmarks
 import path_provider_macos
 import shared_preferences_macos
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  SecureBookmarksPlugin.register(with: registry.registrar(forPlugin: "SecureBookmarksPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
