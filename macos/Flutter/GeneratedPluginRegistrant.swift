0398ba3cca11db2aa88c51215f06d83e43346b77
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-08 00:30:49 +0900

    add file_selector on dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,11 +7,13 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import path_provider_macos
 import shared_preferences_macos
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
