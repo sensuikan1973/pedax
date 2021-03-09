68e49289c26a16b2779809d9e35da63103513ea2
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-10 02:01:22 +0900

    fix script

--- scripts/build_macos_for_distribution.sh
+++ scripts/build_macos_for_distribution.sh
@@ -1,17 +1,23 @@
 # See: https://flutter.dev/desktop#macos
 
+set -e
+
 flutter channel dev
 flutter upgrade
 
 flutter test
+
+flutter clean
 flutter drive --driver integration_test/driver.dart --target integration_test/app_test.dart -d macos
 
 flutter clean
 flutter build macos
+
+git diff --exit-code
+
 open macos/Runner.xcworkspace
 
 # See: https://help.apple.com/xcode/mac/current/#/devac02c5ab8
-
 # Archive
 # Validate
 # Upload
