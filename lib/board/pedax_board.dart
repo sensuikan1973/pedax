644ce609bb8a421004a3f0376ee952b83cfcaaac
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-21 07:09:49 +0900

    book load request

--- lib/board/pedax_board.dart
+++ lib/board/pedax_board.dart
@@ -45,9 +45,15 @@ class _PedaxBoardState extends State<PedaxBoard> {
   void initState() {
     super.initState();
     _boardNotifier = context.read<BoardNotifier>()..requestInit();
-    // _bookFileOption.val.then(_boardNotifier.requestBookLoad);
     _shortcutList = shortcutList(_boardNotifier);
     RawKeyboard.instance.addListener(_handleRawKeyEvent);
+    Future<void>.delayed(
+      const Duration(seconds: 1),
+      () async {
+        final bookFilePath = await _bookFileOption.val;
+        _boardNotifier.requestBookLoad(bookFilePath);
+      },
+    );
   }
 
   @override
