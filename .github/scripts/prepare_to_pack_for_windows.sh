f26a689724cf21034bf54d5cbd69e414b762f0ac
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-20 13:04:58 +0900

    fix

--- .github/scripts/prepare_to_pack_for_windows.sh
+++ .github/scripts/prepare_to_pack_for_windows.sh
@@ -2,10 +2,11 @@
 
 # See: https://flutter.dev/desktop#windows
 # See: https://github.com/sensuikan1973/pedax/pull/71#issuecomment-798849250
+# See: https://github.com/sensuikan1973/pedax/pull/83#issuecomment-803240876
 msvc_path="/c/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Redist/MSVC"
-ls $msvc_path
+ls "$msvc_path"
 
-target_vc_path="$msvc_path/14.28.29325/x64/Microsoft.VC142.CRT"
+target_vc_path="$msvc_path/14.28.29910/x64/Microsoft.VC142.CRT"
 cp "$target_vc_path/vcruntime140.dll" build/windows/runner/Release/
 cp "$target_vc_path/vcruntime140_1.dll" build/windows/runner/Release/
 cp "$target_vc_path/msvcp140.dll" build/windows/runner/Release/
