7683e13cedffb8f2285b928ea48e5d7290c2a91d
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-22 00:45:11 +0900

    fix dll path

--- .github/scripts/prepare_to_pack_for_windows.sh
+++ .github/scripts/prepare_to_pack_for_windows.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# See: https://flutter.dev/desktop#windows
-# See: https://github.com/sensuikan1973/pedax/pull/71#issuecomment-798849250
-# See: https://github.com/sensuikan1973/pedax/pull/83#issuecomment-803240876
-msvc_path="/c/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Redist/MSVC"
-ls "$msvc_path"
+# See: https://flutter.dev/desktop#building-your-own-zip-file-for-windows
+# REF: https://github.com/sensuikan1973/pedax/pull/71#issuecomment-798849250
+# REF: https://github.com/sensuikan1973/pedax/pull/83#issuecomment-803240876
+system32_path="/c/Windows/System32"
+ls "$system32_path"
 
-target_vc_path="$msvc_path/14.28.29910/x64/Microsoft.VC142.CRT"
 dest_path="build/windows/runner/Release/"
-cp "$target_vc_path/vcruntime140.dll" $dest_path
-cp "$target_vc_path/vcruntime140_1.dll" $dest_path
-cp "$target_vc_path/msvcp140.dll" $dest_path
+cp "$system32_path/vcruntime140.dll" $dest_path
+cp "$system32_path/vcruntime140_1.dll" $dest_path
+cp "$system32_path/msvcp140.dll" $dest_path
