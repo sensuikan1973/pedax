675d1fd6b6b92a8f717e579a891e1e40b8554548
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-22 23:59:35 +0900

    ensure window min size

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,25 @@ import 'window.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setWindowMinSize(pedaxWindowMinSize);
+  await ensureMinWindowSize();
 
   // If you feel debug log is noisy, you can change log level.
   // Logger.level = Level.info;
 
   runApp(const PedaxApp());
+}
+
+Future<void> ensureMinWindowSize() async {
+  setWindowMinSize(pedaxWindowMinSize);
+
+  final windowInfo = await getWindowInfo();
+  if (windowInfo.frame.width >= pedaxWindowMinSize.width && windowInfo.frame.height >= pedaxWindowMinSize.height) {
+    return;
+  }
+
+  setWindowFrame(Rect.fromCenter(
+    center: windowInfo.frame.center,
+    width: pedaxWindowMinSize.width,
+    height: pedaxWindowMinSize.height,
+  ));
 }
