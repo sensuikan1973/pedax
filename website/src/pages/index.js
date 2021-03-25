5201e570aed800147af3f7b8702d1f9ec69e1afd
ShimizuNaoki <sensuikan1973@gmail.com>
2021-03-26 04:12:39 +0900

    fix

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -40,6 +40,7 @@ export default function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
+      title={"install page"} /*`${siteConfig.title}`*/
       description="Othello Board with edax <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
