b6968879a7999090c0386f27a58398c24dab9df5
done <23427957+sensuikan1973@users.noreply.github.com>
2021-03-27 01:51:46 +0900

    marketing website by docusaurus (#96)
    * scaffold See: https://docusaurus.io/docs/installation
    * favicon, logo
    * first implement website
    * deploy workflow
    * set github pages url
    * meta:image
    * add icon image on appbar

---
+++ website/docusaurus.config.js
@@ -0,0 +1,49 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'pedax',
+  titleDelimiter: '🍵',
+  tagline: 'Othello Board with edax',
+  url: 'https://sensuikan1973.github.io',
+  baseUrl: '/pedax/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'sensuikan1973',
+  projectName: 'pedax',
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja', 'en'],
+  },
+  themeConfig: {
+    image: 'img/pedax_og.png',
+    navbar: {
+      title: 'pedax',
+      logo: { alt: 'pedax Logo', src: 'img/logo.svg' },
+      items: [
+        {
+          href: 'https://github.com/sensuikan1973/pedax',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'left',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Naoki Shimizu. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ website/src/pages/index.js
@@ -0,0 +1,76 @@
+import Translate, { translate } from '@docusaurus/Translate'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
+import clsx from 'clsx'
+import React from 'react'
+
+import styles from './styles.module.css'
+
+const features = [
+  {
+    title: translate({ id: "analysis_mode_board_title" }),
+    imageUrl: translate({ id: "analysis_mode_board_view" }),
+    description: translate({ id: "analysis_mode_board_description" }),
+  },
+  {
+    title: translate({ id: "custome_options_title" }),
+    imageUrl: translate({ id: "custome_options_view" }),
+    description: translate({ id: "custome_options_description" }),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--6', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3 className="text--center">{title}</h3>
+      <p className="text--center">{description}</p>
+    </div>
+  );
+}
+
+export default function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={ translate({ id: "installation_page_title" }) } /*`${siteConfig.title}`*/
+      description={`${translate({ id: "pedax_description" })} <head />`}>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{translate({ id: "pedax_description" })}</p>
+          {/* <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div> */}
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
