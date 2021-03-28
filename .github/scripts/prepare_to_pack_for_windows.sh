3fc9ad554d80d63cb13c049ae6768c52d95aac6b
done <23427957+sensuikan1973@users.noreply.github.com>
2021-03-28 19:30:40 +0900

    fix packaging script for windows (#105)

--- .github/scripts/prepare_to_pack_for_windows.sh
+++ .github/scripts/prepare_to_pack_for_windows.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+flutter pub run msix:create
+
+# For developers which want to run `.exe` directory, repack ***.dll.
 # See: https://flutter.dev/desktop#building-your-own-zip-file-for-windows
 # REF: https://github.com/sensuikan1973/pedax/pull/71#issuecomment-798849250
 # REF: https://github.com/sensuikan1973/pedax/pull/83#issuecomment-803240876
@@ -10,5 +13,3 @@ dest_path="build/windows/runner/Release/"
 cp "$system32_path/vcruntime140.dll" $dest_path
 cp "$system32_path/vcruntime140_1.dll" $dest_path
 cp "$system32_path/msvcp140.dll" $dest_path
-
-flutter pub run msix:create
