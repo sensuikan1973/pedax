8f7c5c06b1a8672c84ba5be4d9d569ee4cc427fe
done <23427957+sensuikan1973@users.noreply.github.com>
2021-04-27 02:32:51 +0900

    upgrade libedax4dart and the bin (#123)
    * upgrade libedax4dart
    
    * upgrade libeadx bin
    
    * mild fix

--- scripts/fetch_libedax_assets.sh
+++ scripts/fetch_libedax_assets.sh
@@ -1,15 +1,11 @@
-# about macos, See: https://flutter.dev/docs/development/platform-integration/c-interop#compiled-dynamic-library-macos
-
 mkdir -p assets/libedax
-dll_dst=assets/libedax/dll
 data_dst=assets/libedax/data
-mkdir -p $dll_dst
 mkdir -p $data_dst
 
 tmp_dst=libedax_assets_tmp
 mkdir -p $tmp_dst
 
-tag=libedax_assets_4
+tag=libedax_assets_6
 asset_url_prefix=https://github.com/sensuikan1973/libedax4dart/releases/download/$tag
 # See: https://github.com/sensuikan1973/libedax4dart/releases/latest
 
@@ -17,15 +13,19 @@ function unpack_dyamic_library() {
   platform=$1
   asset_url=$2
   lib_name=$3
+  dll_dst=$4
   curl -L $asset_url -o $tmp_dst/${platform}_asset.zip
   unzip $tmp_dst/${platform}_asset.zip -d $tmp_dst/${platform}
   mv $tmp_dst/${platform}/libedax_output/bin/${lib_name} $dll_dst
 }
 
 # Linux dynamic library
-unpack_dyamic_library linux $asset_url_prefix/libedax_Linux.zip libedax.so
+unpack_dyamic_library linux $asset_url_prefix/libedax_Linux.zip libedax.so assets/libedax/dll
 # Windows dynamic library
-unpack_dyamic_library windows $asset_url_prefix/libedax_Windows.zip libedax-x64.dll
+unpack_dyamic_library windows $asset_url_prefix/libedax_Windows.zip libedax-x64.dll assets/libedax/dll
+# MacOS dynamic library
+# See: https://flutter.dev/docs/development/platform-integration/c-interop#compiled-dynamic-library-macos
+unpack_dyamic_library macos $asset_url_prefix/libedax_macOS.zip libedax.dylib macos
 
 # data
 mv $tmp_dst/linux/libedax_output/data/eval.dat $data_dst
