1e038c851681a65b61a6575fe6e0b346a2da15d0
ShimizuNaoki <sensuikan1973@gmail.com>
2021-05-02 18:18:15 +0900

    tmp print

--- test_helper/edax_server.dart
+++ test_helper/edax_server.dart
@@ -3,8 +3,12 @@ import 'package:meta/meta.dart';
 
 @isTest
 Future<void> waitEdaxSetuped(WidgetTester tester) async {
+  print('will delay 1'); // ignore: avoid_print
   await Future<void>.delayed(const Duration(seconds: 1));
+  print('will pump 1'); // ignore: avoid_print
   await tester.pump(); // after spwaning EdaxServer has completed, render UI.
+  print('will delay 2'); // ignore: avoid_print
   await Future<void>.delayed(const Duration(seconds: 1)); // wait EdaxServer execute edax_init.
+  print('will pump 2'); // ignore: avoid_print
   await tester.pump(); // after edax init has completed, render UI.
 }
