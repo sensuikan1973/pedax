43eee6fa70e2a5ac659bdf12e9ede22532e3fd1e
ShimizuNaoki <sensuikan1973@gmail.com>
2021-07-18 21:35:18 +0900

    debug

--- .github/scripts/prepare_to_pack_for_windows.sh
+++ .github/scripts/prepare_to_pack_for_windows.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+set -eu
 
-flutter pub run msix:create
 flutter pub get
+flutter pub run msix:create
 
 # For developers which want to run `.exe` directory, repack ***.dll.
 # See: https://flutter.dev/desktop#building-your-own-zip-file-for-windows
 # REF: https://github.com/sensuikan1973/pedax/pull/71#issuecomment-798849250
 # REF: https://github.com/sensuikan1973/pedax/pull/83#issuecomment-803240876
 system32_path="/c/Windows/System32"
-ls "$system32_path"
+# ls "$system32_path" # debug print
 
 dest_path="build/windows/runner/Release/"
 cp "$system32_path/vcruntime140.dll" $dest_path
