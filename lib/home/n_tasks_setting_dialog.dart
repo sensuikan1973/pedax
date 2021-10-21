983592d950a9ef6e117534f59610b46ea03482f9
ShimizuNaoki <sensuikan1973@gmail.com>
2021-10-21 12:15:24 +0900

    fix analyzer warning

--- lib/home/n_tasks_setting_dialog.dart
+++ lib/home/n_tasks_setting_dialog.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // ignore: depend_on_referenced_packages
 import 'package:provider/provider.dart';
