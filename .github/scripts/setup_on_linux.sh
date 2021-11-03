d29fcefd03b3d285835ca75adf0088c163efe73c
ShimizuNaoki <sensuikan1973@gmail.com>
2021-11-03 17:39:10 +0900

    improve integration test

--- .github/scripts/setup_on_linux.sh
+++ .github/scripts/setup_on_linux.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-flutter config --enable-linux-desktop
+# See: https://flutter.dev/desktop#additional-linux-requirements
 sudo apt-get update -y
 sudo apt-get install -y libgtk-3-dev libx11-dev pkg-config cmake ninja-build libblkid-dev liblzma-dev
+
+# See: https://flutter.dev/desktop#set-up
+flutter config --enable-linux-desktop
