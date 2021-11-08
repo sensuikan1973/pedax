8ebc47f30830696757284876fdba9e8aa74d764f
sensuikan1973 <sensuikan1973@gmail.com>
2021-11-09 08:16:48 +0900

    [ci skip] add documentation link of integration test

--- integration_test/driver.dart
+++ integration_test/driver.dart
@@ -3,6 +3,8 @@
 //
 // flutter drive --driver integration_test/driver.dart --target integration_test/app_test.dart
 
+// See: https://flutter.dev/docs/testing/integration-tests
+
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() async => integrationDriver();
