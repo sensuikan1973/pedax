7fe647bd6ab92b093565ec4445f44b15078db226
sensuikan1973 <sensuikan1973@gmail.com>
2021-11-22 00:29:34 +0900

    mild improve diff check

--- scripts/deploy_macos_app_to_app_store.sh
+++ scripts/deploy_macos_app_to_app_store.sh
@@ -32,9 +32,12 @@ flutter drive --driver integration_test/driver.dart --target integration_test/ap
 flutter clean
 flutter build macos --release
 
+cd macos
+bundle install
+
+cd ..
 git diff --exit-code
 
 cd macos
-bundle install
 export ASC_KEY_CONTENT=$(cat $asc_key_p8_file_path | base64)
 bundle exec fastlane deploy_app_store # require ENV variables
