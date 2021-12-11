96ad7257ac90cc93a2796c8dbaae75b6fd831fce
sensuikan1973 <sensuikan1973@gmail.com>
2021-12-11 23:40:48 +0900

    refactoring, See: https://github.com/flutter/plugins/pull/4547/files#diff-a5ae049a146b414930be866a1128adedf7c2298ed4807d4504d5c462a5146331

--- test/widget_test_helper/libedax_assets.dart
+++ test/widget_test_helper/libedax_assets.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
+import 'package:path_provider_macos/path_provider_macos.dart';
 import 'package:pedax/engine/edax_asset.dart';
 
 @isTest
 Future<void> prepareLibedaxAssets() async {
   // See: https://flutter.dev/docs/cookbook/persistence/reading-writing-files#testing
   final dir = await Directory.systemTemp.createTemp();
-  const MethodChannel('plugins.flutter.io/path_provider_macos').setMockMethodCallHandler((final methodCall) async {
+  // See: https://github.com/flutter/plugins/pull/4547/files#diff-a5ae049a146b414930be866a1128adedf7c2298ed4807d4504d5c462a5146331
+  PathProviderMacOS().methodChannel.setMockMethodCallHandler((final methodCall) async {
     if (methodCall.method == 'getApplicationDocumentsDirectory') return dir.path;
     return null;
   });
