604e926be89200b0f2b9bd7f745bc085649af258
sensuikan1973 <sensuikan1973@gmail.com>
2022-01-28 06:34:11 +0900

    fix mock

--- test/widget_test_helper/file_selector_mock.dart
+++ test/widget_test_helper/file_selector_mock.dart
@@ -2,10 +2,16 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
 
-// See: https://github.com/flutter/plugins/blob/master/packages/file_selector/file_selector_platform_interface/lib/src/method_channel/method_channel_file_selector.dart
 @isTest
 void mockFileSelector() {
+  // See: https://github.com/flutter/plugins/blob/master/packages/file_selector/file_selector_platform_interface/lib/src/method_channel/method_channel_file_selector.dart
   const MethodChannel('plugins.flutter.io/file_selector').setMockMethodCallHandler((final methodCall) async {
+    if (methodCall.method == 'openFile') return null;
+    return null;
+  });
+
+  // See: https://github.com/flutter/plugins/blob/master/packages/file_selector/file_selector_windows/lib/file_selector_windows.dart
+  const MethodChannel('plugins.flutter.io/file_selector_windows').setMockMethodCallHandler((final methodCall) async {
     if (methodCall.method == 'openFile') return null;
     return null;
   });
