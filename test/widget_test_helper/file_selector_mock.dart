6405d226d25ab35fc09870b21aafef8ded44301c
done <23427957+sensuikan1973@users.noreply.github.com>
2022-02-11 20:10:49 +0900

    [Scheduled] `flutter pub upgrade --null-safety` (#330)
    * [Scheduled] `flutter pub upgrade --null-safety`
    
    * change url_launcher mock
    
    Co-authored-by: sensuikan1973 <sensuikan1973@users.noreply.github.com>

--- test/widget_test_helper/file_selector_mock.dart
+++ test/widget_test_helper/file_selector_mock.dart
@@ -2,17 +2,19 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
 
+const _methodChannels = [
+  // See: https://github.com/flutter/plugins/blob/master/packages/file_selector/file_selector_platform_interface/lib/src/method_channel/method_channel_file_selector.dart
+  MethodChannel('plugins.flutter.io/file_selector'),
+  // See: https://github.com/flutter/plugins/blob/master/packages/file_selector/file_selector_windows/lib/file_selector_windows.dart
+  MethodChannel('plugins.flutter.io/file_selector_windows'),
+];
+
 @isTest
 void mockFileSelector() {
-  // See: https://github.com/flutter/plugins/blob/master/packages/file_selector/file_selector_platform_interface/lib/src/method_channel/method_channel_file_selector.dart
-  const MethodChannel('plugins.flutter.io/file_selector').setMockMethodCallHandler((final methodCall) async {
-    if (methodCall.method == 'openFile') return null;
-    return null;
-  });
-
-  // See: https://github.com/flutter/plugins/blob/master/packages/file_selector/file_selector_windows/lib/file_selector_windows.dart
-  const MethodChannel('plugins.flutter.io/file_selector_windows').setMockMethodCallHandler((final methodCall) async {
-    if (methodCall.method == 'openFile') return null;
-    return null;
-  });
+  for (final channel in _methodChannels) {
+    channel.setMockMethodCallHandler((final methodCall) async {
+      if (methodCall.method == 'openFile') return null;
+      return null;
+    });
+  }
 }
