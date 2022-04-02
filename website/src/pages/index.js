ea0415354dbf274b7ebb1e0ba8e1f22c918db0d6
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-03 05:20:00 +0900

    fix typo custome_options_title -> custom_options_title

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -9,19 +9,19 @@ import styles from './styles.module.css'
 
 const features = [
   {
-    title: translate({ id: "analysis_mode_board_title" }),
-    imageUrl: translate({ id: "analysis_mode_board_view" }),
-    description: translate({ id: "analysis_mode_board_description" }),
+    title: translate({ id: 'analysis_mode_board_title' }),
+    imageUrl: translate({ id: 'analysis_mode_board_view' }),
+    description: translate({ id: 'analysis_mode_board_description' }),
   },
   {
-    title: translate({ id: "custome_options_title" }),
-    imageUrl: translate({ id: "custome_options_view" }),
-    description: translate({ id: "custome_options_description" }),
+    title: translate({ id: 'custom_options_title' }),
+    imageUrl: translate({ id: 'custome_options_view' }),
+    description: translate({ id: 'custome_options_description' }),
   },
-];
+]
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl)
   return (
     <div className={clsx('col col--6', styles.feature)}>
       {imgUrl && (
@@ -32,23 +32,28 @@ function Feature({imageUrl, title, description}) {
       <h3 className="text--center">{title}</h3>
       <p className="text--center">{description}</p>
     </div>
-  );
+  )
 }
 
 export default function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const context = useDocusaurusContext()
+  const { siteConfig = {} } = context
   return (
     <Layout
-      title={ translate({ id: "installation_page_title" }) } /*`${siteConfig.title}`*/
-      description={`${translate({ id: "pedax_description" })} <head />`}>
+      title={translate({ id: 'installation_page_title' })} /*`${siteConfig.title}`*/
+      description={`${translate({ id: 'pedax_description' })} <head />`}
+    >
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{translate({ id: "pedax_description" })}</p>
+          <p className="hero__subtitle">{translate({ id: 'pedax_description' })}</p>
           <section className="storeImages">
-            <a href={ translate({ id: "mac_app_store_link" }) } rel="noreferrer" target="_blank"><img className={styles.storeImage} src={ translate({ id: "mac_app_store_badge" }) }/></a>
-            <a href={ translate({ id: "microsoft_store_link" }) } rel="noreferrer" target="_blank"><img className={styles.storeImage} src={ translate({ id: "microsoft_store_badge" }) }/></a>
+            <a href={translate({ id: 'mac_app_store_link' })} rel="noreferrer" target="_blank">
+              <img className={styles.storeImage} src={translate({ id: 'mac_app_store_badge' })} />
+            </a>
+            <a href={translate({ id: 'microsoft_store_link' })} rel="noreferrer" target="_blank">
+              <img className={styles.storeImage} src={translate({ id: 'microsoft_store_badge' })} />
+            </a>
           </section>
         </div>
       </header>
@@ -66,5 +71,5 @@ export default function Home() {
         )}
       </main>
     </Layout>
-  );
+  )
 }
