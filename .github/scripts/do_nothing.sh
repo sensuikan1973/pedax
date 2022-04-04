20ef2b322af43ac9398638a6ac606eec7be6a8c0
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-05 05:14:52 +0900

    mild fix

--- .github/scripts/do_nothing.sh
+++ .github/scripts/do_nothing.sh
@@ -6,6 +6,7 @@ do nothing.
 
 See:
 - https://github.com/sensuikan1973/pedax/issues/415
+- https://github.com/sensuikan1973/pedax/settings/branch_protection_rules/19119198
 - https://docs.github.com/ja/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/troubleshooting-required-status-checks#handling-skipped-but-required-checks
 - https://docs.github.com/ja/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/automatically-merging-a-pull-request
 
