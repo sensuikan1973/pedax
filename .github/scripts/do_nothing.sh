8decfeafbd431d1697cd655f1ff56b2e75557b47
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-05 05:21:11 +0900

    mild fix echo message

--- .github/scripts/do_nothing.sh
+++ .github/scripts/do_nothing.sh
@@ -10,7 +10,7 @@ See:
 - https://docs.github.com/ja/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/troubleshooting-required-status-checks#handling-skipped-but-required-checks
 - https://docs.github.com/ja/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/automatically-merging-a-pull-request
 
-why do you use reusable workflow ?
+Can I use reusable workflow ? -> No.
 https://docs.github.com/ja/actions/using-workflows/reusing-workflows#limitations at 2022/04/05 says
 "The strategy property is not supported in any job that calls a reusable workflow."
 MSG
