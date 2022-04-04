3ab9970729f6ed023bf9138aacb747113907c16b
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-05 05:08:58 +0900

    mild fix echo message

--- .github/scripts/do_nothing.sh
+++ .github/scripts/do_nothing.sh
@@ -3,9 +3,11 @@ set -euxo pipefail
 
 echo <<MSG
 do nothing.
-# See:
-# - https://docs.github.com/ja/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/troubleshooting-required-status-checks#handling-skipped-but-required-checks
-# - https://docs.github.com/ja/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/automatically-merging-a-pull-request
+
+See:
+- https://github.com/sensuikan1973/pedax/issues/415
+- https://docs.github.com/ja/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/troubleshooting-required-status-checks#handling-skipped-but-required-checks
+- https://docs.github.com/ja/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/automatically-merging-a-pull-request
 
 why do you use reusable workflow ?
 https://docs.github.com/ja/actions/using-workflows/reusing-workflows#limitations at 2022/04/05 says
