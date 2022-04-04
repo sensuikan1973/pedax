a132f53fe267125461fb3ea443cd7ca37f53bf5d
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-05 05:06:52 +0900

    fix

--- .github/scripts/do_nothing.sh
+++ .github/scripts/do_nothing.sh
@@ -1,16 +1,13 @@
-# See: https://docs.github.com/ja/actions/using-workflows/reusing-workflows
+#!/bin/bash
+set -euxo pipefail
 
+echo <<MSG
+do nothing.
 # See:
 # - https://docs.github.com/ja/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/troubleshooting-required-status-checks#handling-skipped-but-required-checks
 # - https://docs.github.com/ja/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/automatically-merging-a-pull-request
-name: always success
 
-on:
-  workflow_call:
-  workflow_dispatch:
-
-jobs:
-  do_nothing:
-    runs-on: ubuntu-latest
-    steps:
-      - run: 'echo "passed See: .github/workflows/README.md" '
+why do you use reusable workflow ?
+https://docs.github.com/ja/actions/using-workflows/reusing-workflows#limitations at 2022/04/05 says
+"The strategy property is not supported in any job that calls a reusable workflow."
+MSG
