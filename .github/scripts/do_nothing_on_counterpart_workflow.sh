1afac8dcd266a1f824def5a8093c41972aa47903
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-05 05:30:32 +0900

    fix message

--- .github/scripts/do_nothing_on_counterpart_workflow.sh
+++ .github/scripts/do_nothing_on_counterpart_workflow.sh
@@ -2,15 +2,23 @@
 set -euxo pipefail
 
 cat <<MSG
-do nothing for protected branch.
+do nothing on counterpart workflow for protected branch.
 
-See:
-- https://github.com/sensuikan1973/pedax/issues/415
-- https://github.com/sensuikan1973/pedax/settings/branch_protection_rules/19119198
-- https://docs.github.com/ja/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/troubleshooting-required-status-checks#handling-skipped-but-required-checks
-- https://docs.github.com/ja/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/automatically-merging-a-pull-request
+[FAQ]
+Q. What the purpose of this script ?
+A. I want to enable auto-merge with keeping code quality.
+  See:
+  - https://docs.github.com/ja/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/troubleshooting-required-status-checks#handling-skipped-but-required-checks
+  - https://github.com/sensuikan1973/pedax/issues/415
+  - https://github.com/sensuikan1973/pedax/settings/branch_protection_rules/19119198
+  - https://docs.github.com/ja/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/automatically-merging-a-pull-request
 
-Can I use reusable workflow ? -> No.
-https://docs.github.com/ja/actions/using-workflows/reusing-workflows#limitations at 2022/04/05 says
-"The strategy property is not supported in any job that calls a reusable workflow."
+
+Q. Can I use reusable workflow ?
+A. No.
+  https://docs.github.com/ja/actions/using-workflows/reusing-workflows#limitations at 2022/04/05 says
+  "The strategy property is not supported in any job that calls a reusable workflow."
+
+Q. What is the origin of `cointerpart` ?
+A. See: https://docs.github.com/ja/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/troubleshooting-required-status-checks#handling-skipped-but-required-checks
 MSG
