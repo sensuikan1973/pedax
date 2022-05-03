fb018d24521549ecbc34e0808cd2f67cc1548d91
sensuikan1973 <sensuikan1973@gmail.com>
2022-05-03 14:48:21 +0900

    use snap on linux

--- .github/scripts/linux/setup.sh
+++ .github/scripts/linux/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-# See: https://flutter.dev/desktop#additional-linux-requirements
+# See: https://docs.flutter.dev/desktop#additional-linux-requirements
 sudo apt-get update -y
-sudo apt-get install -y libgtk-3-dev libx11-dev pkg-config cmake ninja-build libblkid-dev liblzma-dev
+sudo snap install flutter --classic
 
 # See: https://flutter.dev/desktop#set-up
 flutter config --enable-linux-desktop
