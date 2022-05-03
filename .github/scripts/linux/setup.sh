8c1402c216425bba0bba779f1356a3b7d2d4f1e6
sensuikan1973 <sensuikan1973@gmail.com>
2022-05-03 14:54:04 +0900

    fix setting up linux

--- .github/scripts/linux/setup.sh
+++ .github/scripts/linux/setup.sh
@@ -3,7 +3,8 @@ set -euxo pipefail
 
 # See: https://docs.flutter.dev/desktop#additional-linux-requirements
 sudo apt-get update -y
-sudo snap install flutter --classic
+sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
+
 
 # See: https://docs.flutter.dev/desktop#set-up
 flutter config --enable-linux-desktop
