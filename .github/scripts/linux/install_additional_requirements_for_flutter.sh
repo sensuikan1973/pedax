de986f44047bd6f5747d47b9695b32838753479a
sensuikan1973 <sensuikan1973@gmail.com>
2022-05-07 05:22:46 +0900

    change script name

--- .github/scripts/linux/install_additional_requirements_for_flutter.sh
+++ .github/scripts/linux/install_additional_requirements_for_flutter.sh
@@ -4,7 +4,3 @@ set -euxo pipefail
 # See: https://docs.flutter.dev/desktop#additional-linux-requirements
 sudo apt-get update -y
 sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
-
-
-# See: https://docs.flutter.dev/desktop#set-up
-flutter config --enable-linux-desktop
---
+++ .github/scripts/macos/install_additional_requirements_for_flutter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euxo pipefail
+
+# See: https://docs.flutter.dev/desktop#additional-macos-requirements
+echo "No additional requirements."
