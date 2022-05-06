b0d8f07eae8a82c2508f8eb1d828f9f629ba04ff
done <23427957+sensuikan1973@users.noreply.github.com>
2022-05-07 05:56:04 +0900

    remove redundant script on flutter_build workflow (#510)
    * remove redundant script on flutter_build workflow
    
    * mild change the order of step
    
    * change script name
    
    * follow https://github.com/sensuikan1973/pedax/pull/510#issuecomment-1119984005
    
    * change diff check step order

--- .github/scripts/macos/install_additional_requirements_for_flutter.sh
+++ .github/scripts/macos/install_additional_requirements_for_flutter.sh
@@ -2,6 +2,4 @@
 set -euxo pipefail
 
 # See: https://docs.flutter.dev/desktop#additional-macos-requirements
-
-# See: https://docs.flutter.dev/desktop#set-up
-flutter config --enable-macos-desktop
+echo "No additional requirements."
--- .github/scripts/windows/install_additional_requirements_for_flutter.sh
+++ .github/scripts/windows/install_additional_requirements_for_flutter.sh
@@ -2,6 +2,4 @@
 set -euxo pipefail
 
 # See: https://docs.flutter.dev/desktop#additional-windows-requirements
-
-# See: https://docs.flutter.dev/desktop#set-up
-flutter config --enable-windows-desktop
+echo "No additional requirements."
