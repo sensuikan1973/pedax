294bd25e7e002e8219b01e0f1dcdaba7a997f5f4
sensuikan1973 <sensuikan1973@gmail.com>
2022-05-14 03:33:58 +0900

    FakeUrlLauncher

---
+++ test/widget_test_helper/fake_url_launcher.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:meta/meta.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
+
+@isTest
+void mockUrlLauncher() {
+  UrlLauncherPlatform.instance = FakeUrlLauncher();
+}
+
+@isTest
+class FakeUrlLauncher extends Fake
+    with
+        MockPlatformInterfaceMixin // ignore: prefer_mixin
+    implements
+        UrlLauncherPlatform {
+  @override
+  Future<bool> canLaunch(String url) async => false;
+
+  @override
+  Future<bool> launch(
+    String url, {
+    required bool useSafariVC,
+    required bool useWebView,
+    required bool enableJavaScript,
+    required bool enableDomStorage,
+    required bool universalLinksOnly,
+    required Map<String, String> headers,
+    String? webOnlyWindowName,
+  }) async =>
+      false;
+}
