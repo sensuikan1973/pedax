5891397cbcb2df04b0dc2099b7940bcc613075f3
sensuikan1973 <sensuikan1973@gmail.com>
2022-07-16 20:07:57 +0900

    remove tech documentation link

--- lib/home/setting_dialogs/bestpath_count_availability_setting_dialog.dart
+++ lib/home/setting_dialogs/bestpath_count_availability_setting_dialog.dart
@@ -2,15 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // ignore: depend_on_referenced_packages
 import 'package:provider/provider.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../engine/options/pedax/bestpath_count_availability_option.dart';
 import '../../models/board_notifier.dart';
-
-final _documentationUrlOfBestpathCount = Uri.https(
-  'sensuikan1973.github.io',
-  'libedax4dart/libedax4dart/LibEdax/edaxBookCountBoardBestpath.html',
-);
 
 class BestpathCountAvailabilitySettingDialog extends StatelessWidget {
   BestpathCountAvailabilitySettingDialog({super.key});
@@ -33,18 +27,6 @@ class BestpathCountAvailabilitySettingDialog extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     Text(AppLocalizations.of(context)!.bestpathCountAvailabilityDescription),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (await canLaunchUrl(_documentationUrlOfBestpathCount)) {
-                          await launchUrl(_documentationUrlOfBestpathCount);
-                        }
-                      },
-                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white)),
-                      child: Text(
-                        AppLocalizations.of(context)!.bestpathCountAvailabilityDocumentationLink,
-                        style: const TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
-                      ),
-                    ),
                     const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
                     Switch(
                       value: _enabled.value!,
