9f64dea98418e2b2981d7573e39eb7472e0c803e
sensuikan1973 <sensuikan1973@gmail.com>
2022-12-04 07:17:40 +0900

    remove comment

--- test/widget_test_helper/mock_package_info.dart
+++ test/widget_test_helper/mock_package_info.dart
@@ -1,7 +1,6 @@
 import 'package:meta/meta.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-// See: https://github.com/authpass/macos_secure_bookmarks/blob/master/lib/macos_secure_bookmarks.dart
 @isTest
 void mockPackageInfo() {
   PackageInfo.setMockInitialValues(
