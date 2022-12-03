fd14d98f2e000a0f9c7fd7e7fbe94e97a93e3767
done <23427957+sensuikan1973@users.noreply.github.com>
2022-12-04 03:10:41 +0900

    Update prepare_to_pack.sh

--- .github/scripts/windows/prepare_to_pack.sh
+++ .github/scripts/windows/prepare_to_pack.sh
@@ -6,7 +6,7 @@ echo "See: https://docs.flutter.dev/desktop#windows"
 flutter pub run msix:create
 
 # For developers which want to run `.exe` directly, I repack ***.dll.
-# See: https://docs.flutter.dev/desktop#building-your-own-zip-file-for-windows
+# See: https://docs.flutter.dev/development/platform-integration/windows/building#building-your-own-zip-file-for-windows
 # REF: https://github.com/sensuikan1973/pedax/pull/71#issuecomment-798849250
 # REF: https://github.com/sensuikan1973/pedax/pull/83#issuecomment-803240876
 system32_path="/c/Windows/System32"
