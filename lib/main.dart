241151f000c7783a530be808ca498e6c24a62f50
done <23427957+sensuikan1973@users.noreply.github.com>
2023-01-06 00:39:21 +0900

    mild refactoring (#1168)

--- lib/main.dart
+++ lib/main.dart
@@ -14,46 +14,23 @@ const pedaxWindowMinSize = Size(550, 680);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Logger.level = Level.nothing; // If you feel debug log is noisy, you can change log level.
+  await _setupWindow();
+  await _runAppWithSentry();
+}
 
+Future<void> _setupWindow() async {
   // https://github.com/sensuikan1973/pedax/issues/1159
   await windowManager.ensureInitialized(); // https://github.com/leanflutter/window_manager/tree/v0.2.9#usage
   windowManager.waitUntilReadyToShow(null, () async {
     setWindowMinSize(pedaxWindowMinSize); // https://github.com/flutter/flutter/issues/30736
-    await _setWindowFrame();
+    await _changeWindowFrame();
     await windowManager.show();
     await windowManager.focus();
   });
-
-  // If you feel debug log is noisy, you can change log level.
-  // Logger.level = Level.info;
-
-  // https://docs.sentry.io/platforms/flutter/usage/#tips-for-catching-errors
-  // https://docs.flutter.dev/testing/errors#errors-caught-by-flutter
-  FlutterError.onError = (errorDetails) async {
-    Logger().e(errorDetails.exception);
-    await Sentry.captureException(errorDetails.exception, stackTrace: errorDetails.stack);
-  };
-
-  // https://github.com/getsentry/sentry-dart/tree/6.17.0/flutter#usage
-  // https://docs.flutter.dev/testing/errors#errors-not-caught-by-flutter
-  PlatformDispatcher.instance.onError = (error, stack) {
-    Sentry.captureException(error, stackTrace: stack);
-    return true;
-  };
-  await SentryFlutter.init(
-    (options) {
-      // https://docs.sentry.io/platforms/flutter/#configure
-      // https://pub.dev/documentation/sentry/latest/sentry_io/SentryOptions-class.html
-      options
-        ..dsn = const String.fromEnvironment('SENTRY_DSN') // ignore: do_not_use_environment
-        ..tracesSampleRate = 1.0
-        ..debug = kDebugMode;
-    },
-    appRunner: () => runApp(const PedaxApp()),
-  );
 }
 
-Future<void> _setWindowFrame() async {
+Future<void> _changeWindowFrame() async {
   final windowInfo = await getWindowInfo();
   setWindowFrame(
     Rect.fromLTWH(
@@ -64,3 +41,33 @@ Future<void> _setWindowFrame() async {
     ),
   );
 }
+
+Future<void> _runAppWithSentry() async {
+  // https://docs.sentry.io/platforms/flutter/usage/#tips-for-catching-errors
+  // https://docs.flutter.dev/testing/errors#errors-caught-by-flutter
+  FlutterError.onError = (errorDetails) async {
+    Logger().e(errorDetails.exception);
+    await Sentry.captureException(errorDetails.exception, stackTrace: errorDetails.stack);
+  };
+
+  // https://github.com/getsentry/sentry-dart/tree/6.18.1/flutter#usage
+  // https://docs.flutter.dev/testing/errors#errors-not-caught-by-flutter
+  PlatformDispatcher.instance.onError = (error, stack) {
+    Sentry.captureException(error, stackTrace: stack);
+    return true;
+  };
+
+  await SentryFlutter.init(
+    (options) {
+      // https://docs.sentry.io/platforms/flutter/#configure
+      // https://pub.dev/documentation/sentry/latest/sentry_io/SentryOptions-class.html
+      options
+        ..dsn = const String.fromEnvironment('SENTRY_DSN') // ignore: do_not_use_environment
+        ..tracesSampleRate = 1.0
+        ..debug = kDebugMode;
+    },
+    appRunner: () => _runApp(),
+  );
+}
+
+Future<void> _runApp() async => runApp(const PedaxApp());
