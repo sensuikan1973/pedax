3cbd56280a868e6c667f32aad56a0026d7dd26f1
sensuikan1973 <sensuikan1973@gmail.com>
2023-01-06 00:26:04 +0900

    mild refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,35 @@ const pedaxWindowMinSize = Size(550, 680);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Logger.level = Level.nothing; // If you feel debug log is noisy, you can change log level.
+  await _setupWindow();
+  await _runAppWithSentry();
+}
 
+Future<void> _setupWindow() async {
   // https://github.com/sensuikan1973/pedax/issues/1159
   await windowManager.ensureInitialized(); // https://github.com/leanflutter/window_manager/tree/v0.2.9#usage
   windowManager.waitUntilReadyToShow(null, () async {
     setWindowMinSize(pedaxWindowMinSize); // https://github.com/flutter/flutter/issues/30736
-    await _setWindowFrame();
+    await _changeWindowFrame();
     await windowManager.show();
     await windowManager.focus();
   });
+}
 
-  // If you feel debug log is noisy, you can change log level.
-  // Logger.level = Level.info;
+Future<void> _changeWindowFrame() async {
+  final windowInfo = await getWindowInfo();
+  setWindowFrame(
+    Rect.fromLTWH(
+      await PedaxApp.savedWindowFrameLeft ?? windowInfo.frame.left,
+      await PedaxApp.savedWindowFrameTop ?? windowInfo.frame.top,
+      await PedaxApp.savedWindowFrameWidth ?? pedaxWindowMinSize.width,
+      await PedaxApp.savedWindowFrameHeight ?? pedaxWindowMinSize.height,
+    ),
+  );
+}
 
+Future<void> _runAppWithSentry() async {
   // https://docs.sentry.io/platforms/flutter/usage/#tips-for-catching-errors
   // https://docs.flutter.dev/testing/errors#errors-caught-by-flutter
   FlutterError.onError = (errorDetails) async {
@@ -40,6 +56,7 @@ Future<void> main() async {
     Sentry.captureException(error, stackTrace: stack);
     return true;
   };
+
   await SentryFlutter.init(
     (options) {
       // https://docs.sentry.io/platforms/flutter/#configure
@@ -49,18 +66,8 @@ Future<void> main() async {
         ..tracesSampleRate = 1.0
         ..debug = kDebugMode;
     },
-    appRunner: () => runApp(const PedaxApp()),
+    appRunner: () => _runApp(),
   );
 }
 
-Future<void> _setWindowFrame() async {
-  final windowInfo = await getWindowInfo();
-  setWindowFrame(
-    Rect.fromLTWH(
-      await PedaxApp.savedWindowFrameLeft ?? windowInfo.frame.left,
-      await PedaxApp.savedWindowFrameTop ?? windowInfo.frame.top,
-      await PedaxApp.savedWindowFrameWidth ?? pedaxWindowMinSize.width,
-      await PedaxApp.savedWindowFrameHeight ?? pedaxWindowMinSize.height,
-    ),
-  );
-}
+Future<void> _runApp() async => runApp(const PedaxApp());
