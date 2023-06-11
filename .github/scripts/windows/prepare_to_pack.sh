e02dd220e06a2a91a1a7bfa844a46411c661393c
done <23427957+sensuikan1973@users.noreply.github.com>
2023-06-12 08:26:10 +0900

    「Deprecated. Use `dart run` instead.」 (#1520)

--- .github/scripts/windows/prepare_to_pack.sh
+++ .github/scripts/windows/prepare_to_pack.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 echo "See: https://docs.flutter.dev/development/platform-integration/windows/building"
 
-flutter pub run msix:create
+dart run msix:create
 
 # For developers which want to run `.exe` directly, I repack ***.dll.
 # See: https://docs.flutter.dev/development/platform-integration/windows/building#building-your-own-zip-file-for-windows
