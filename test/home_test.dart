c6a066f8dec5f17d56146ac3b7778a853c4010e4
sensuikan1973 <sensuikan1973@gmail.com>
2023-11-16 14:56:29 +0900

    flutter analyze

--- test/home_test.dart
+++ test/home_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // ignore: depend_on_referenced_packages
 import 'package:flutter_test/flutter_test.dart';
