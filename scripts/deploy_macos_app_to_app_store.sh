0ec85cda754be032659495da063ce5f81b801846
sensuikan1973 <sensuikan1973@gmail.com>
2024-02-23 23:25:23 +0900

    drop unused script option

--- scripts/deploy_macos_app_to_app_store.sh
+++ scripts/deploy_macos_app_to_app_store.sh
@@ -10,7 +10,7 @@ set -euxo pipefail
 # shellcheck disable=SC2168
 local -A opthash
 # See: https://zsh.sourceforge.io/Doc/Release/Zsh-Modules.html#The-zsh_002fzutil-Module
-zparseopts -D -F -A opthash -- -dry-run -skip-test revision:
+zparseopts -D -F -A opthash -- -dry-run revision:
 
 if [[ -z "${opthash[(i)-revision]}" ]]; then
   echo "revision is required"
@@ -22,11 +22,6 @@ git checkout "${opthash[-revision]}"
 
 # shellcheck disable=SC1091
 source ./scripts/setup_flutter.sh
-
-if [[ -z "${opthash[(i)--skip-test]}" ]]; then
-  flutter test --concurrency=1
-  flutter test integration_test --device-id macos
-fi
 
 # See: https://flutter.dev/desktop#macos
 flutter build macos --release --dart-define SENTRY_DSN="$SENTRY_DSN"
