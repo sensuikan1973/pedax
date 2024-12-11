756a626f1cea417b72e8e56fb5ff785c84a418d8
sensuikan1973 <23427957+sensuikan1973@users.noreply.github.com>
2024-12-11 19:23:38 +0900

    `flutter pub upgrade --major-versions --tighten` (#2357)
    * flutter pub upgrade --major-versions --tighten
    
    * See: tter_dependencies origin/upgrade_flutter_dependencies
    Switched to a new bra
    
    * hey
    
    ---------
    
    Co-authored-by: sensuikan1973 <sensuikan1973@users.noreply.github.com>

--- .github/scripts/linux/install_additional_requirements_for_flutter.sh
+++ .github/scripts/linux/install_additional_requirements_for_flutter.sh
@@ -3,4 +3,9 @@ set -euxo pipefail
 
 # See: https://docs.flutter.dev/platform-integration/desktop#additional-linux-requirements
 sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
 sudo apt-get install -y clang cmake git ninja-build pkg-config libgtk-3-dev liblzma-dev libstdc++-12-dev
+
+# https://github.com/sensuikan1973/pedax/pull/2357#issuecomment-2535409742
+sudo apt install -y libcurl4-openssl-dev
