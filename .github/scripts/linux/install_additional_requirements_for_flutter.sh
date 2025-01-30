a1b79420fc973a2636e5a1e4981f57feaccf092d
sensuikan1973 <sensuikan1973@gmail.com>
2025-01-30 12:31:42 +0900

    [skip ci] update doc link

--- .github/scripts/linux/install_additional_requirements_for_flutter.sh
+++ .github/scripts/linux/install_additional_requirements_for_flutter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-# See: https://docs.flutter.dev/platform-integration/desktop#additional-linux-requirements
+# See: https://docs.flutter.dev/get-started/install/linux/desktop
 sudo apt-get update -y
 sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
 sudo apt-get install -y clang cmake git ninja-build pkg-config libgtk-3-dev liblzma-dev libstdc++-12-dev
