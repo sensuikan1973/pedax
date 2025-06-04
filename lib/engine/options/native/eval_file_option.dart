e9a9476aaf90c509194b57ab2245cc7b6b654b98
sensuikan1973 <sensuikan1973@gmail.com>
2025-06-05 00:39:55 +0900

    fix scpecified -> specified

--- lib/engine/options/native/eval_file_option.dart
+++ lib/engine/options/native/eval_file_option.dart
@@ -37,7 +37,7 @@ class EvalFileOption implements EngineNativeOption<String> {
     final pref = await _preferences;
     if (val.isEmpty) {
       final newPath = await appDefaultValue;
-      Logger().i('scpecified path is empty. So, pedax sets $newPath.');
+      Logger().i('specified path is empty. So, pedax sets $newPath.');
       await pref.setString(prefKey, newPath);
       return newPath;
     } else {
