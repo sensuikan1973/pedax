91e4915905acfc661c1d632d6cd2fa9df194697e
sensuikan1973 <sensuikan1973@gmail.com>
2025-06-05 00:32:23 +0900

    fix cleaning macos security scope

--- lib/engine/options/native/book_file_option.dart
+++ lib/engine/options/native/book_file_option.dart
@@ -80,8 +80,10 @@ class BookFileOption implements EngineNativeOption<String> {
 
   Future<void> stopAccessingSecurityScopedResource() async {
     if (!Platform.isMacOS) return;
+
     if (_macosBookmarkResolvedFile != null) {
       await SecureBookmarks().stopAccessingSecurityScopedResource(_macosBookmarkResolvedFile!);
+      _macosBookmarkResolvedFile = null;
     }
   }
 
