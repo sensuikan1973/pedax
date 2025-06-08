95d9c7d861932092db671c73a7b4be12f8bcda3a
sensuikan1973 <sensuikan1973@gmail.com>
2025-06-08 10:29:52 +0900

    fix typo

--- scripts/fetch_libedax_assets.sh
+++ scripts/fetch_libedax_assets.sh
@@ -18,7 +18,7 @@ tag=$(flutter pub deps --style compact | grep "libedax4dart" | awk -F ' ' '{prin
 asset_url_prefix=https://github.com/sensuikan1973/libedax4dart/releases/download/$tag
 # See: https://github.com/sensuikan1973/libedax4dart/releases/latest
 
-function unpack_dyamic_library() {
+function unpack_dynamic_library() {
   platform=$1
   asset_url=$2
   lib_name=$3
@@ -29,12 +29,12 @@ function unpack_dyamic_library() {
 }
 
 # Linux dynamic library
-unpack_dyamic_library linux "$asset_url_prefix/Linux.zip" libedax.so assets/libedax/dll
+unpack_dynamic_library linux "$asset_url_prefix/Linux.zip" libedax.so assets/libedax/dll
 # Windows dynamic library
-unpack_dyamic_library windows "$asset_url_prefix/Windows.zip" libedax-x64.dll assets/libedax/dll
+unpack_dynamic_library windows "$asset_url_prefix/Windows.zip" libedax-x64.dll assets/libedax/dll
 # MacOS dynamic library
 # See: https://flutter.dev/docs/development/platform-integration/c-interop#compiled-dynamic-library-macos
-unpack_dyamic_library macos "$asset_url_prefix/macOS.zip" libedax.universal.dylib macos
+unpack_dynamic_library macos "$asset_url_prefix/macOS.zip" libedax.universal.dylib macos
 
 # data
 mv "$tmp_dst/linux/libedax_output/data/eval.dat" "$data_dst"
