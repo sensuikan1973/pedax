59406f64a2e079f652e130ebad1c9e5f8dc28566
sensuikan1973 <sensuikan1973@gmail.com>
2025-06-20 19:40:07 +0900

    fix

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -1,45 +1,13 @@
 #!/bin/bash
 
-# Flutter SDK のバージョン (必要に応じて変更してください)
-FLUTTER_VERSION="3.19.0" # 例: 最新の安定版などを指定
-# Flutter SDK を展開するディレクトリ (ユーザーのホームディレクトリ配下の .flutter_sdk とします)
+# https://docs.flutter.dev/get-started/install/linux/desktop#install-the-flutter-sdk
+# https://docs.flutter.dev/install/archive#main-channel
+
 FLUTTER_SDK_DIR="$HOME/.flutter_sdk"
-FLUTTER_INSTALL_DIR="$FLUTTER_SDK_DIR/flutter"
 
-echo "Flutter SDK のセットアップを開始します..."
+git clone -b stable https://github.com/flutter/flutter.git "$FLUTTER_SDK_DIR"
 
-echo "Flutter SDK を $FLUTTER_SDK_DIR にダウンロードして展開します..."
-# ディレクトリが存在しない場合は作成
-mkdir -p "$FLUTTER_SDK_DIR"
-cd "$FLUTTER_SDK_DIR"
+echo 'export PATH="$PATH:$FLUTTER_SDK_DIR/flutter/bin"' >> ~/.bashrc
+source ~/.bashrc
 
-# Flutter SDK をダウンロード (Linux x64 の場合)
-# アーキテクチャが異なる場合は、適切な URL に変更してください
-# 最新版は公式サイトで確認してください: https://flutter.dev/docs/get-started/install/linux#install-flutter-manually
-FLUTTER_ARCHIVE="flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
-wget "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/$FLUTTER_ARCHIVE"
-
-# ダウンロードしたアーカイブを展開
-tar xf "$FLUTTER_ARCHIVE"
-
-# 不要になったアーカイブファイルを削除
-rm "$FLUTTER_ARCHIVE"
-
-echo "Flutter SDK の展開が完了しました。"
-
-# PATH に Flutter SDK の bin ディレクトリを追加
-# この設定は現在のセッションでのみ有効です。
-# 永続的に設定するには、~/.bashrc や ~/.zshrc などに追記してください。
-export PATH="$PATH:$FLUTTER_INSTALL_DIR/bin"
-
-echo "PATH に Flutter SDK を追加しました: $PATH"
-
-# Flutter Doctor を実行してセットアップ状況を確認
-echo "Flutter Doctor を実行します..."
 flutter doctor
-
-echo ""
-echo "Flutter SDK のセットアップが完了しました。"
-echo "永続的にPATHを設定するには、お使いのシェルの設定ファイル (例: ~/.bashrc, ~/.zshrc) に以下を追記してください:"
-echo "  export PATH="\$PATH:$FLUTTER_INSTALL_DIR/bin""
-echo "設定変更後は、新しいターミナルを開くか、'source ~/.bashrc' (または該当ファイル) を実行してください。"
