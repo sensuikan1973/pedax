84eb2e3707dd1f8dbbec557162d36ba4ea0c13f4
sensuikan1973 <23427957+sensuikan1973@users.noreply.github.com>
2026-03-03 00:09:05 +0900

    Update path.dart

--- lib/windows/path.dart
+++ lib/windows/path.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:path/path.dart' as p;
 
-final _windowsAsciiPathPattern = RegExp(r'^([ -~]|[¥])+$');
+final _windowsAsciiPathPattern = RegExp(r'^([ -~]|[¥])+$'); // https://stackoverflow.com/a/14608823
 
 bool isWindowsAsciiPath(final String path) => _windowsAsciiPathPattern.hasMatch(path);
 
